27c83e66ee738810cf97fbdd70032c7b4b822d72
gagal18 <gagaleskibojan@gmail.com>
2021-04-25 20:15:27 +0200

    a

--- src/containers/toDo/toDo.js
+++ src/containers/toDo/toDo.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const toDo = (props) => {
     const name = props.user.displayName
+    const id = props.user.uid
     console.log('TODO',props.user)
+    console.log(id)
     return(
         <h1>{name}<br/>You are signed in</h1>
     )
